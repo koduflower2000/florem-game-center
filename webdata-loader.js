20dbace788af9128809576d2d20c3dc38746ffa1
kodu/kevin <kavinthirukt42@gmail.com>
2025-01-23 13:31:15 +0530

    Add files via upload

---
+++ webdata-loader.js
@@ -0,0 +1,24 @@
+const cacheName = 'cache-loader';
+const assetsToCache = [
+  './',
+  './index.html',
+  './css/',
+  './img/',
+  // other assets
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(assetsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
